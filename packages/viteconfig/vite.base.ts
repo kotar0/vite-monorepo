f8db28e5f6e1908104a41e6415faf212f207f1cf
Kotaro Okuya <koutaro.okuya@gmail.com>
2023-03-07 13:43:37 +0900

    ADD vitest and testing library

--- packages/viteconfig/vite.base.ts
+++ packages/viteconfig/vite.base.ts
@@ -2,4 +2,9 @@ import react from '@vitejs/plugin-react-swc';
 
 export const baseConfig = {
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './vitest.setup.ts',
+  },
 };
